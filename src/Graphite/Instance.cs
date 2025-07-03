c9d5b0a5cfd30e6495ac5dafaef76bc0c1b5a312
Aqua <ollie@ollierobinson.co.uk>
2025-07-03 15:54:46 +0100

    start instances

---
+++ src/Graphite/Instance.cs
@@ -0,0 +1,13 @@
+﻿using Graphite.Vulkan;
+
+namespace Graphite;
+
+public abstract class Instance : IDisposable
+{
+    public abstract void Dispose();
+
+    public static Instance Create(in InstanceInfo info)
+    {
+        return new VulkanInstance(in info);
+    }
+}
---
+++ src/Graphite/InstanceInfo.cs
@@ -0,0 +1,8 @@
+namespace Graphite;
+
+public struct InstanceInfo
+{
+    public string AppName;
+
+    public bool Debug;
+}
