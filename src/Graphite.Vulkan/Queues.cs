630625a2dab93c35288ef89c2d3cc36fdd36a6c3
Aqua <ollie@ollierobinson.co.uk>
2025-11-29 23:35:25 +0000

    queues

---
+++ src/Graphite.Vulkan/Queues.cs
@@ -0,0 +1,14 @@
+﻿using Silk.NET.Vulkan;
+
+namespace Graphite.Vulkan;
+
+internal struct Queues
+{
+    public uint GraphicsIndex;
+    public uint PresentIndex;
+
+    public Queue Graphics;
+    public Queue Present;
+
+    public HashSet<uint> UniqueQueues => [GraphicsIndex, PresentIndex];
+}
--- src/Graphite.Vulkan/VulkanDevice.cs
+++ src/Graphite.Vulkan/VulkanDevice.cs
@@ -7,11 +7,19 @@ internal sealed class VulkanDevice : Device
     public override bool IsDisposed { get; protected set; }
 
     private readonly Vk _vk;
+    private readonly VkInstance _instance;
 
+    public readonly PhysicalDevice PhysicalDevice;
     public readonly VkDevice Device;
+
+    public readonly Queues Queues;
 
     public VulkanDevice(Vk vk, VkInstance instance, VulkanSurface surface, PhysicalDevice physicalDevice)
     {
+        _vk = vk;
+        _instance = instance;
+        PhysicalDevice = physicalDevice;
+        
         
     }
     
